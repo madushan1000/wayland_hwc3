763742cc2c7f143436d34483f582d8f032066ce3
Madhushan Nishantha <madhushan.nishantha@researchgate.net>
2024-02-25 20:21:19 +0100

    poc wayland client on linux, broken on android due to shmem

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use async_trait::async_trait;
 use binder_tokio::*;
-use std::{io, string};
+use std::{borrow::Borrow, io, string};
 use tokio::task;
+use wayrs_client::{
+    self,
+    global::GlobalsExt,
+    protocol::{wl_shm, WlCompositor, WlSurface},
+    EventCtx,
+};
+use wayrs_protocols::xdg_shell::{
+    xdg_surface, xdg_toplevel, xdg_wm_base, XdgSurface, XdgToplevel, XdgWmBase,
+};
+use wayrs_utils::shm_alloc::{self, ShmAlloc};
 
 #[allow(non_snake_case)]
 use android_hardware_graphics_composer3::aidl::android::hardware::graphics::composer3::IComposer;
@@ -52,13 +62,87 @@ async fn start_hwc3() {
     });
 }
 
+struct WaylandClientState {
+}
+
 async fn start_wayland_client() {
     println!("starting wayland client");
+
+    let (mut conn, globals) =
+        wayrs_client::Connection::<WaylandClientState>::async_connect_and_collect_globals()
+            .await
+            .expect("can't connect to wayland socket");
+
+    let wl_compositor: WlCompositor = globals.bind(&mut conn, 1..=5).expect("no compositor api");
+
+    let mut shm_alloc = ShmAlloc::bind(&mut conn, &globals).expect("ShmAlloc error");
+
+    let shm = shm_alloc
+        .alloc_buffer(
+            &mut conn,
+            shm_alloc::BufferSpec {
+                width: 100,
+                height: 200,
+                stride: 100 * 4,
+                format: wl_shm::Format::Argb8888,
+            },
+        )
+        .expect("can't allocate shm");
+    let xdg_wm_base: XdgWmBase = globals
+        .bind_with_cb(&mut conn, ..=4, xdg_wm_base_cb)
+        .unwrap();
+
+    let wl_surface = wl_compositor.create_surface(&mut conn);
+    let xdg_surface = xdg_wm_base.get_xdg_surface(&mut conn, wl_surface);
+    let xdg_toplevel = xdg_surface.get_toplevel(&mut conn);
+
+    conn.set_callback_for(xdg_surface, |ctx| {
+        if let xdg_surface::Event::Configure(serial) = ctx.event {
+            ctx.proxy.ack_configure(ctx.conn, serial);
+        }
+    });
+
+    conn.set_callback_for(xdg_toplevel, |ctx| match ctx.event {
+        xdg_toplevel::Event::Configure(args) => {
+            //todo
+        }
+        xdg_toplevel::Event::Close => {
+            ctx.conn.break_dispatch_loop();
+        }
+        _ => (),
+    });
+
+    xdg_toplevel.set_app_id(&mut conn, wayrs_client::cstr!("test").into());
+    xdg_toplevel.set_title(&mut conn, wayrs_client::cstr!("TEST").into());
+    
+    for b in shm.1.iter_mut() {
+        *b = 255;
+    }
+    
+    wl_surface.attach(&mut conn, Some(shm.0.into_wl_buffer()), 0, 0);
+    wl_surface.damage(&mut conn, 0, 0, i32::MAX, i32::MAX);
+
+    wl_surface.commit(&mut conn);
+
+    let mut wcstate = WaylandClientState {
+    };
+
+    loop {
+        conn.async_flush().await.expect("can't flush");
+        conn.async_recv_events().await.expect("can't recv events");
+        conn.dispatch_events(&mut wcstate)
+    }
+}
+
+fn xdg_wm_base_cb(ctx: wayrs_client::EventCtx<WaylandClientState, XdgWmBase>) {
+    if let xdg_wm_base::Event::Ping(serial) = ctx.event {
+        ctx.proxy.pong(ctx.conn, serial);
+    }
 }
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 2)]
 async fn main() {
     println!("starting wayland_hwc3 {}", std::process::id());
-    start_hwc3().await;
+    //start_hwc3().await;
     start_wayland_client().await;
 }
